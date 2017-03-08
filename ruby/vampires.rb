05a77e9d837c2b743f3fa60f8d8c0772fdfb772c
Saham <saham.khozestani@att.net>
2017-03-08 12:27:12 -0600

    Add questions to file

---
+++ ruby/vampires.rb
@@ -0,0 +1,27 @@
+# List of famous Vampires
+famous_vampires = ["Highgate", "Gorglin Grange", "Vampire of New England", "Fritz Haarmann", "Clara Geisslerin", "Peter Plogojowitz", "Elizabeth Bathory", "Vlad III"]
+puts " What is your name? "
+name = gets.chomp
+
+# Age
+puts "How old are you? "
+age = gets.chomp.to_i
+
+=begin
+if n age and a year of birth don't line up mathmetically, that employee is a vampire.
+=end
+puts " What year were you born? "
+year = gets.chomp.to_i
+
+=begin
+  Vampires hate garlic. 
+  
+=end
+puts "Our company cafeteria serves garlic bread. Should we order some for you? "
+garlic_bread = gets.chomps
+=begin
+  Vampires are immortal.
+  
+=end
+puts "Would you like to enroll in the company's health insurance? "
+health_insurance = get.chomps
