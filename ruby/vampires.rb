a4b10df915b8d34c9e250a7c0bcb641f8af0aa41
Saham <saham.khozestani@att.net>
2017-03-10 09:47:22 -0600

    add abort to method

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,14 +5,25 @@ when "yes", "Yes", "y", "Y"
 
 when "no", "No", "n","N"
   answer_string = false
+
+ else
+ 	abort("Please use a valid input.")
   
 end
 end
 
-
-
+=begin
+#-----------------------------------------------------------------------------#
+#main program
+Release1: Interview with the Vampire
+Collect information about the new employee
+Name, age, year, do they like garlic, do they need heath insurance
+Save Data.
+#
+=end
 # List of famous Vampires
 famous_vampires = ["Highgate", "Gorglin Grange", "Vampire of New England", "Fritz Haarmann", "Clara Geisslerin", "Peter Plogojowitz", "Elizabeth Bathory", "Vlad III"]
+Puts "Welcome to Werewolf Inc., quittin' time is always before sundown"
 puts " What is your name? "
 name = gets.chomp
 
@@ -26,6 +37,7 @@ if n age and a year of birth don't line up mathmetically, that employee is a vam
 puts " What year were you born? "
 year = gets.chomp.to_i
 
+# get current year
 time = Time.new
 current_year = time.year
 puts current_year
