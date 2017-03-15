58f5612b3883d190881d28eee604321cff0f8d31
Saham <saham.khozestani@att.net>
2017-03-15 09:48:01 -0500

    add puts for hash and array values prior to executing command

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -64,34 +64,42 @@ letters = {
   d: 4
 }
 puts "reject less than 5 in array"
+puts "numbers array prior to command: #{numbers}"
 new_numbers = numbers.reject {|x| x < 5}
 p new_numbers
 
 puts "rejects value less than 5 in hash"
+puts "letters hash prior to reject command: #{letters}"
 new_letters = letters.reject {|x, y| y < 3}
 p new_letters
 
 puts "selects less than 5 in array"
+puts "array prior to select command: #{numbers}"
 new_numbers = numbers.select {|x| x < 5}
 p new_numbers
 
-puts "select value less than 5 in hash"
+puts "select value less than 3 in hash"
+puts "hash prior to select command: #{letters}"
 new_letters = letters.select {|x, y| y < 3}
 p new_letters
 
 puts "keeps if less than 6"
+puts "array prior to keep_if command: #{numbers}"
 numbers.keep_if {|x| x < 6}
 p numbers
 
 puts "keep if value doesn't equal 3"
+puts "hash prior to keep_if command: #{letters}"
 letters.keep_if {|x, y| y != 3}
 p letters
 
 puts "drop while less than 2"
+puts " numbers array prior to drop_while: #{numbers}"
 new_numbers = numbers.drop_while {|x| x < 2}
 p new_numbers
 
 puts "drop while value is equal to 1"
+puts "letters hash prior to drop_while: #{letters}"
 new_letters = letters.drop_while {|x, y| y == 1}
 p new_letters
 
