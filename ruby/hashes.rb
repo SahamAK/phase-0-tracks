d69a0e2acfb72d917dcddb747b18372a3fe8177f
Saham <saham.khozestani@att.net>
2017-03-16 12:00:01 -0500

    add code for release 3 in hashes.rb

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -25,3 +25,30 @@ client_info[:income]=gets.chomp.to_i
 
 puts client_info
 
+puts " "
+
+puts "Client Detail"
+puts "Name: #{client_info[:name]}"
+puts "Age: #{client_info[:age]}"
+puts "Number of children: #{client_info[:num_of_children]}"
+puts "Decore Theme: #{client_info[:decore_theme]}"
+puts "Income: #{client_info[:income]}"
+
+puts " Please enter a key if you wish to change the value: (key)"
+key_entered = gets.chomp
+
+
+if key_entered.empty? == false
+  key_entered = key_entered.to_sym
+  puts key_entered
+  puts "enter new value:"
+  new_value = gets.chomp
+
+
+if new_value.match(/^(\d)+$/) then new_value = new_value.to_i end
+p new_value
+
+client_info[key_entered] = new_value
+puts client_info
+
+end
