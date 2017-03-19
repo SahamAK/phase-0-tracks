cec83dffe1d9164eac9725d952b2d38d39a6cdb2
Saham <saham.khozestani@att.net>
2017-03-18 22:27:19 -0500

    clean up

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,24 +1,22 @@
 
 def tricky_algorithm(name)
+
+#define vowel array and constants 
 vowel = ["a", "e", "i", "o", "u","A","E","I","O","U"]
 non_vowel = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"]
 
-#a = "Felicia Torres"
-
+#split the array to words to rotate first and last name
 new_a =name.split
-p "split the new_a : #{new_a}"
+#p "split the new_a : #{new_a}"
 new_a = new_a.rotate!
-p "new_a after rotation : #{new_a}"
+
+#p "new_a after rotation : #{new_a}"
 new_a = new_a.join(' ').split('')
-
-
 
 #get length of string
 a_length = name.length
-p new_a
-
-
-# for the rest of the letters
+#p new_a
+# for the letters
 # if the letter is a vowel replace with the next vowel except "u"="a"
 # if the letter is constant replace with the next constant, letters before vowel in alphabt
 # will be replaced with the letter after that vowel in the english alphabet
@@ -27,7 +25,7 @@ p new_a
 i=0
 
 new_a.each do |element|
-  p element
+  #p element
   if vowel.include?element 
     index = vowel.find_index(element)
     new_a[i] =vowel[index +1]
@@ -41,9 +39,9 @@ new_a.each do |element|
         new_a[i] = "A"
       end
     end
-    p "vowel = #{new_a[i]}"
+    #p "vowel = #{new_a[i]}"
   elsif non_vowel.include? element
-    p element
+    #p element
 
     if element =="Z" 
       new_a[i] = "B"
@@ -53,40 +51,45 @@ new_a.each do |element|
     else
 
       new_a[i] = element.next
-      p "element next: #{element}"
+      #p "element next: #{element}"
       if vowel.include?new_a[i]
         new_a[i]= new_a[i].next
       end
     end
 
   end
-  p new_a
+  #p new_a
   i+=1
 end
 
-p new_a
-p new_a.join
+#p new_a
+new_a.join
 end
+
+#*******************************************
+#main
+#*****************************************
 arr_name = []
 arr_tricky = []
-p "arr name before pushing : #{arr_name}"
+#p "arr name before pushing : #{arr_name}"
 name = nil
 #Release 1 Provide user interface
 p "Welcome to our Tricky Algorithm to alter your name"
 
  while name !="quit" do
-  p "please enter a first name and last name. If done, enter 'quit':"
+
+  p "Please enter a first name and last name. If done, enter 'quit':"
   name = gets.chomp
 #name = "Saham Khozestani"
-if name != nil 
-  if name !="quit" 
-  arr_tricky<< tricky_algorithm(name)
-  arr_name.push name
+  if name != nil 
+    if name !="quit" 
+    arr_tricky<< tricky_algorithm(name)
+    arr_name.push name
+    end
+  else
+    exit
   end
-else
-  exit
-end
-
+  p " "
 end
 
 #arr_tricky = arr_tricky.compact
@@ -102,6 +105,3 @@ while counter < arr_name.count do
 end
   counter +=1
 end
-
-#p "the array of names: #{arr_name}"
-#p "the array of tricky Names: #{arr_tricky}"
