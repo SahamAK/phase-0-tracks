77142297b0f7d81161c0cbd0ca4043d6a5c8d927
Saham <saham.khozestani@att.net>
2017-03-18 21:04:08 -0500

    add checking for Aa & Zz

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,13 +1,11 @@
+
+def tricky_algorithm(name)
 vowel = ["a", "e", "i", "o", "u","A","E","I","O","U"]
 non_vowel = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"]
 
-a = "Felicia Torres"
-#copy string and lower cases
-#new_a = a.downcase
-#convert strig to an array
+#a = "Felicia Torres"
 
-
-new_a =a.split
+new_a =name.split
 p "split the new_a : #{new_a}"
 new_a = new_a.rotate!
 p "new_a after rotation : #{new_a}"
@@ -16,14 +14,9 @@ new_a = new_a.join(' ').split('')
 
 
 #get length of string
-a_length = a.length
+a_length = name.length
 p new_a
-#replace first letter with last letter
-#new_a[0] = new_a.last
-#replace last letter with first letter
-#new_a[a_length - 1] = a[0].downcase
 
-#p new_a
 
 # for the rest of the letters
 # if the letter is a vowel replace with the next vowel except "u"="a"
@@ -39,22 +32,40 @@ new_a.each do |element|
     index = vowel.find_index(element)
     new_a[i] =vowel[index +1]
     #new_a[i]= element.next
-    if element !="u"
+    if element !="u" ||element !="U"
      new_a[i] = vowel[index + 1]
     else
+      if element =="u"
       new_a[i] = "a"
+      else
+        new_a[i] = "A"
+      end
     end
+    p "vowel = #{new_a[i]}"
   elsif non_vowel.include? element
     p element
-    new_a[i] = element.next
-    p "element next: #{element}"
-    if vowel.include?new_a[i]
-      new_a[i]= new_a[i].next
+
+    if element =="Z" 
+      new_a[i] = "A"
+    elsif element =="z"
+      new_a[i] = "a"
+
+    else
+
+      new_a[i] = element.next
+      p "element next: #{element}"
+      if vowel.include?new_a[i]
+        new_a[i]= new_a[i].next
+      end
     end
 
   end
+  p new_a
   i+=1
 end
 
 p new_a
 p new_a.join
+end
+name = "Saham Khozestani"
+tricky_algorithm(name)
