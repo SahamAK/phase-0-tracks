07c2881df09fd7884a46dfaf85bd9bf6360c42d8
Saham <saham.khozestani@att.net>
2017-03-21 18:04:43 -0500

    Add release 1 code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,7 @@
 class Santa
+  
 
-	def speak
+  def speak
     p "Ho, ho, ho! Haaaappy holidays!"
   end
 
@@ -8,12 +9,19 @@ class Santa
     p "That is a good #{cookie}"
   end
 
-  def initialize
+  def initialize(gender,ethnicity)
     p "Initializing Santa instance ..."
+    @gender= gender
+    @ethnicity= ethnicity
+    @reindeer_ranking= ["Rudolph", "Dasher", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age= 0
   end
-
+  def print_list
+    puts "Gender is #{@gender}"
+    puts "Ethnicity is #{@ethnicity}"
+  end
 end
-
+=begin
 marvin = Santa.new
 deadly = Santa.new
 lastly = Santa.new
@@ -21,3 +29,35 @@ lastly = Santa.new
 
 init_it = marvin.speak
 marvin.eat_milk_and_cookies("snickerdoodle")
+=end
+santa = []
+santa <<Santa.new("agender","black")
+santa << Santa.new("female", "Latino")
+santa << Santa.new("bigender", "white")
+santa << Santa.new("male", "Japanese")
+santa << Santa.new("female", "prefer not to say")
+santa << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santa << Santa.new("N/A", "N/A")
+santa.each do |person|
+  puts "Santa detail"
+  person.print_list
+end
+santa_ladies = []
+details = {
+  "agender": "black",
+  "female": "Latino",
+  "bigender": "white",
+  "male": "Japanese-African",
+  "female": "prefer not to say",
+  "gender fluid": "Mystical Creature (unicorn)",
+  "N/A": "N/A"
+}
+santas = []
+details.each do |key, value|
+  #p "#{key}"
+
+  santas<<Santa.new(key, value)
+end
+santas.each do |person|
+  person.print_list
+end
