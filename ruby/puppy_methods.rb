fd34bc6e34cba024859e19dbb0dab645afd62f84
Saham <saham.khozestani@att.net>
2017-03-22 11:20:42 -0500

    add any method to file

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -10,6 +10,23 @@ class Puppy
   def roll_over
   	p "*rolls over*"
   end
+  def dog_years(human_year)
+  	 dog_year = human_year * 7
+  end
+  # take age as input and calculate the hours of sleep
+  def sleep(age)
+  	# age is in months
+  	
+  	if age < 12
+  		hours = 12
+  	elsif age == (13..24)
+  		hours = 6 
+  	else
+  		hours =16
+  	end
+  	p "If age is #{age} in months, sleeps for #{hours} hours."
+  	hours
+  end
 
 end
 
@@ -20,3 +37,14 @@ p "#{rio.fetch("ball")}"
 p "#{rio.speak(3)}"
 
 p "#{rio.roll_over}"
+ 
+p "#{rio.dog_years(2)}"
+
+p "please enter the age of the dog in months:"
+
+age = gets.chomp.to_i
+
+hours = rio.sleep(age)
+p " hours : #{hours}"
+
+
