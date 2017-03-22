8c1725ce7956aa91368067667fe0a07f69cb59f1
Saham <saham.khozestani@att.net>
2017-03-22 10:58:47 -0500

    add code for methods in puppy.rb

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,22 @@
 class Puppy
 
   def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+    p "I brought back the #{toy}!"
+      toy
+  end
+  def speak(count)
+  	count.times { p "Woof!"}
+  end
+  def roll_over
+  	p "*rolls over*"
   end
 
 end
 
+rio = Puppy.new
+
+p "#{rio.fetch("ball")}"
+
+p "#{rio.speak(3)}"
+
+p "#{rio.roll_over}"
