97181281c37a49e686771f342fcd175c4e9739c6
Saham <saham.khozestani@att.net>
2017-03-23 13:41:50 -0500

    add comments for criteria section

--- ruby/list/todolist.rb
+++ ruby/list/todolist.rb
@@ -17,3 +17,15 @@ class TodoList
   end
 end
 
+=begin 
+This section is for answers to the Criteria section:
+ This criterion is linked to a Learning OutcomeIdentify the components of an Rspec test
+# RSpec
+# 'describe' block for each group of tests  
+  # 'it' block for each individual test
+    # expect(<YOUR CODE>).to eq <RESULT>
+
+Derive class requirements from a set of Rspec tests
+  describe is the section where you initialize the calss. 
+  it will give you the methods from the class. This also gives you the structure of the method.
+  expect gives you the expected output from each method.
