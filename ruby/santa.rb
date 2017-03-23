00772e2d098dc7897df9610b82ba19def57448d7
Saham <saham.khozestani@att.net>
2017-03-22 22:30:15 -0500

    add the new methods celebrate_birthday, get_mad_at, getters and setter for age

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -20,6 +20,30 @@ class Santa
     puts "Gender is #{@gender}"
     puts "Ethnicity is #{@ethnicity}"
   end
+  #increase age by 1 year
+  def celebrate_birthday
+    @age +=1
+  end
+  #get the reindeer name and move to the end of the array .
+  def get_mad_at(reindeer_name)
+    index = @reindeer_ranking.index(reindeer_name)
+    @reindeer_ranking.delete_at(index)
+    @reindeer_ranking.insert(@reindeer_ranking.length, reindeer_name)
+    p "#{@reindeer_ranking}"
+  end
+  #setter to set the gender to a new value
+  def gender=(new_gender)
+    @gender = new_gender
+    p "#{@gender}"
+  end
+  #getter to get the value of age
+  def age
+    @age
+  end
+  #getter to get ethnicity
+  def ethnicity
+    @ethnicity
+  end
 end
 =begin
 marvin = Santa.new
@@ -42,7 +66,7 @@ santa.each do |person|
   puts "Santa detail"
   person.print_list
 end
-santa_ladies = []
+=begin
 details = {
   "agender": "black",
   "female": "Latino",
@@ -50,7 +74,9 @@ details = {
   "male": "Japanese-African",
   "female": "prefer not to say",
   "gender fluid": "Mystical Creature (unicorn)",
-  "N/A": "N/A"
+  "N/A": "N/A",
+  "male": "Martian",
+  "child": "R2D2"
 }
 santas = []
 details.each do |key, value|
@@ -62,6 +88,20 @@ santas.each do |person|
   person.print_list
 end
 
+santas.each  do |person|
+  p "#{person.eat_milk_and_cookies("chocolate chip cookie")}"
+end
+=end
+
 #Release 2
 #getters and setters
+age = santa[1].celebrate_birthday
+p "age is #{age}"
+get_mad = santa[1].get_mad_at("Dasher")
+p "santa #{santa[1].print_list}"
+
+santa[1].gender = ("Unique")
+
+p "get age and ethnicity of 3rd element in santa array"
+p "male age: #{santa[3].age} and Ethnicity: #{santa[3].ethnicity}."
 
