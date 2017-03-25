458d4b041003c775a8a9f4adf788cbe10cd03e48
Saham <saham.khozestani@att.net>
2017-03-25 11:49:28 -0500

    add calls to modules for the two classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,6 +1,6 @@
 # Name: Saham Khozestani
 # 6.4 Modules
-#require_relative 'Shout'
+
 # module Shout
 #   #we'll put some methods here soon, but this code is fine for now!
 #   def self.yell_angrily(words)
@@ -15,6 +15,7 @@
 # word_happy = "Awesome"
 # p "#{Shout.yell_angrily(word_angry)}"
 # p "#{Shout.yelling_happily(word_happy)}"
+# Release 3: Convert a Standalone Module to a Mixin
 
 module Shout
 	def yell_angrily(words)
@@ -38,5 +39,7 @@ adult = Adult.new
 children = Children.new
 
 p "And he said: #{adult.yell_angrily("What the ...")}"
+p "When they won, he said #{adult.yelling_happily("We did it !")}"
 
-p "And she said: #{children.yelling_happily("Yes. I did it!")}"
+p "The little girl said: #{children.yelling_happily("Yes. I did it!")}"
+p "Then when she got upset,she said: #{children.yell_angrily("I hate this")}"
