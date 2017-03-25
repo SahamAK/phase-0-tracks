9a26a17354c6621e08e80c08623aedf06fd23475
Saham <saham.khozestani@att.net>
2017-03-25 11:36:37 -0500

    shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,18 +1,42 @@
 # Name: Saham Khozestani
 # 6.4 Modules
 #require_relative 'Shout'
+# module Shout
+#   #we'll put some methods here soon, but this code is fine for now!
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yelling_happily(words)
+#     words + "Yehhhhhhhhhhhh..." + " :-))))))"
+#   end
+# end
+
+# word_angry = "Fudge"
+# word_happy = "Awesome"
+# p "#{Shout.yell_angrily(word_angry)}"
+# p "#{Shout.yelling_happily(word_happy)}"
+
 module Shout
-  #we'll put some methods here soon, but this code is fine for now!
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-  def self.yelling_happily(words)
-    words + "Yehhhhhhhhhhhh..." + " :-))))))"
-  end
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yelling_happily(words)
+		words + " Yehhhhhhhhhh" + ":-))))))"
+	end
 end
 
-word_angry = "Fudge"
-word_happy = "Awesome"
-p "#{Shout.yell_angrily(word_angry)}"
-p "#{Shout.yelling_happily(word_happy)}"
+class Adult
+	include Shout
+end
 
+class Children
+	include Shout
+end
+
+adult = Adult.new
+children = Children.new
+
+p "And he said: #{adult.yell_angrily("What the ...")}"
+
+p "And she said: #{children.yelling_happily("Yes. I did it!")}"
