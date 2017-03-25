fbb17281b6bc16cb58b9cb02e2ec2d610e56884c
Saham <saham.khozestani@att.net>
2017-03-24 19:04:34 -0500

    added cases to test get_letter method

--- ruby/word/word_game.rb
+++ ruby/word/word_game.rb
@@ -2,23 +2,26 @@
 #Name: Saham Khozestani
 #
 class WordGame
-  attr_reader :word
-  attr_reader :guess_count
-  attr_reader :is_over
+  attr_reader :game_word 
+  attr_writer :guess_word
+  attr_accessor :guess_count
+  attr_accessor :is_over
 
   def initialize(word)
     @game_word = word
+    p "#{@guess_word}"
+    @guess_word = "-----"
     @guess_count = word.length
     @is_over = false
   end
 
   def initial_state(word_guess)
-    if word_guess.length == 0
+    if @guess_word.length == 0
       @game_word.length.times do |i|
-        word_guess << "-"
+        @guess_word << "-"
       end
     end
-    word_guess
+    @guess_word
   end
 
   def current_state(word_guess)
@@ -27,6 +30,34 @@ class WordGame
 
   def get_letter(letter)
     p "get letter"
+    p "game word is not: #{@game_word}"
+    p "guess wordkis before : #{@guess_word}"
+    p "#{letter}"
+    temp=@game_word.split(//)
+    p "gamae word: #{temp}"
+    if (temp.include? (letter))
+       @guess_word.split(/ /)
+       p "This is guess word : #{@guess_word}"
+      temp.each do |i|
+         #p @guess_word[i]
+        if i == letter
+          temp.index(i)
+
+          @guess_word[temp.index(i)] = letter 
+         end
+       end
+    @guess_count -= 1
+    end
+    str = "a..zA..Z\-"
+    word = ""
+    
+    # @guess_word.each do |c| 
+    #   if c.chr =~ (/[a-zA-Z\-]/) 
+    #   word <<c
+    #   end 
+    # end
+    p "guess word: #{@guess_word}"
+    @guess_word
   end
 
   def print_result(word_guess)
--- ruby/word/word_game_spec.rb
+++ ruby/word/word_game_spec.rb
@@ -2,10 +2,15 @@
 #Name: Saham Khozestani
 #
 require_relative "word_game"
+
 describe WordGame do 
+	let(:wordgame) { WordGame.new("glass") }
 	it "initialize with the new word" do
-		wordgame = WordGame.new("glass")
+		#wordgame = WordGame.new("glass")
 		expect(wordgame.initial_state("")).to eq "-----"
+	end
+	it "add letter to the new string" do
+		expect(wordgame.get_letter("l")).to eq "-l---"
 	end
 	
 end
