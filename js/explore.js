aff70852ad7232ef9b459cce28c40bda6c81267f
SahamAK <saham.khozestani@att.net>
2017-03-29 16:37:33 -0500

    explore.js

--- js/explore.js
+++ js/explore.js
@@ -2,16 +2,16 @@
 //Pseudocode
 // This function will take in a a string as a parameter and reverse the string.
 // read the string.
-// for each character fron the end of the string
+// for each character fronm the end of the string
 //   put the character in a new string
 //   decrement counter
 // until counter is =0.
 
 
 function reverse(str){
-var str_2 ;
+var str_2 = "" ;
 
-for (var i = str.length; i>=0; i--){
+for (var i = (str.length -1); i>=0; i--){
   str_2 += str[i];
 }
 
@@ -19,6 +19,8 @@ return str_2;
 }
 
 var strNotEmpty = true;
+
+//If string is not empty then call reverse and print the output.
 
 if (strNotEmpty){
 console.log(reverse("hello"));
