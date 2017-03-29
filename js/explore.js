8d9125199533a5091ea7cf60842ab964fad0799c
SahamAK <saham.khozestani@att.net>
2017-03-29 16:26:17 -0500

    add driver code and conditional statement

--- js/explore.js
+++ js/explore.js
@@ -6,20 +6,21 @@
 //   put the character in a new string
 //   decrement counter
 // until counter is =0.
-var str = "hello";
 
-// function add(x, y) {
-//   return x + y;
-// }
+
 function reverse(str){
 var str_2 ;
 
-for (var i=str.length; i>=0; i--){
+for (var i = str.length; i>=0; i--){
   str_2 += str[i];
-  //console.log(str_2);
 }
-//console.log(str_2);
+
 return str_2;
 }
+
+var strNotEmpty = true;
+
+if (strNotEmpty){
 console.log(reverse("hello"));
+}
 
