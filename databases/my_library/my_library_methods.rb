38d121b4f45d2f4e73b773366490d2687e70d825
Saham <saham.khozestani@att.net>
2017-04-09 22:35:23 -0500

    add methods to update tables, add new book and author

--- databases/my_library/my_library_methods.rb
+++ databases/my_library/my_library_methods.rb
@@ -1,19 +1,7 @@
-# Create the database for my library
+#My_library methods are group here.
+#Work in progress
 
-
-#this will check for a duplicate row in a table
-# def check_duplicate_owner(db,value)
-#   duplicate_length = db.execute("Select * from book_owners where owner_name = ?",value).length
-#   while duplicate_length > 0
-#     db.execute('DELETE FROM book_owners WHERE owner_name =?', value)
-#     duplicate_length -= 1
-#     return true
-#   else 
-#     return false 
-#   end 
-# end 
-
-#This will add a new book to the my_books
+#This method will add a new book to the my_books
 def add_new_book(db,book_name)
   db.execute('INSERT INTO my_books (book_title) VALUES (?)', book_name)
 end
@@ -30,11 +18,17 @@ end
 def update_owner(db,new_owner,id)
   db.execute("UPDATE book_owners SET owner_name = ? WHERE owner_id = ?",new_owner,id)
 end
+#add author to tbl
+def add_author(db,author)
+  db.execute('INSERT INTO authors (author_name) VALUES (?)', author)
+end
 
+
+# This will give the user an option from the main menu and will continue executing until they enter 0 to end program
 def option
   input = nil
   while choice_check(input) == false
-    puts "[1] book [2] owner [3] reader [4] wish list [0] end"
+    puts "[1] update tables [2] book [3] owner [4] reader [5] wish list [0] end"
 
     #puts "Would you like to [1] View your watchlist [2] Add a symbol to your watchlist [3] Remove a symbol from your watchlist [0] Exit from your watchlist."
     input = gets.chomp
@@ -44,6 +38,28 @@ def option
 end
 
 def choice_check(input)
+  options = ['1','2','3','4','5','0']
+  return options.include?(input) 
+end
+
+#This is the submenu for adding details to the following tables:
+#book_condition,type_of_book,is_book_ready_for_donation, genre
+def tbls_data
+  input = nil
+  while choice_tbl(input) == false
+    puts "Please pick a table to update:"
+    puts "[1] type_of_book"
+    puts "[2] genre"
+    puts "[3] book_condition"
+    puts "[4] is_book_ready_for_donation"
+    puts "[0] done"
+    input = gets.chomp
+    puts "Invalid selection. Please enter a valid selection" if choice_tbl(input) == false
+  end
+  return input
+end
+
+def choice_tbl(input)
   options = ['1','2','3','4','0']
   return options.include?(input) 
 end
